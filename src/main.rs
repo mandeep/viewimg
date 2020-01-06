9e7180295f73da469368a987b81cf792718d5f98
mandeep <mandeep@users.noreply.github.com>
2020-01-05 18:30:27 -0600

    Pass only the file name to the render loop

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,7 @@ mod render;
 use render::render;
 
 fn main() {
-    let (width, height) = (800u32, 800u32);
-
     let file = env::args().skip(1).next().expect("Error: Please provide a valid HDR image path.");
 
-    render(file, width, height).unwrap();
+    render(file).unwrap();
 }
