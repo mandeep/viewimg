62d0f3fe4889cf6dc4c6447889813e07dd39f790
mandeep <mandeep@users.noreply.github.com>
2020-01-11 20:05:11 -0600

    Change error message of non provided filepath for clarity

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,11 @@ mod render;
 use render::render;
 
 fn main() {
-    let file = env::args().skip(1).next().expect("Error: Please provide a valid HDR image path.");
+    let file = env::args()
+        .skip(1)
+        .next()
+        .expect("ERROR: A filepath to an image must be provided as a commandline argument.");
+
 
     render(file).unwrap();
 }
