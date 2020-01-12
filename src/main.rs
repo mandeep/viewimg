f18d2c82170bacca675d55a4ab3d87610ba5c631
mandeep <mandeep@users.noreply.github.com>
2020-01-11 20:06:19 -0600

    Handle errors from image and filepath

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
 use std::env;
+use std::path::Path;
+
+use image2::io;
 
 mod render;
 use render::render;
@@ -9,6 +12,21 @@ fn main() {
         .next()
         .expect("ERROR: A filepath to an image must be provided as a commandline argument.");
 
+    let filepath = Path::new(&file);
 
-    render(file).unwrap();
+    if filepath.is_file() {
+        if let Ok(image) = io::read(filepath) {
+            if let Err(error) = render(image, filepath) {
+                eprintln!("{}", error);
+                std::process::exit(1);
+            }
+        } else {
+            eprintln!("ERROR: Failed to read image. Please make sure it is a valid image file.");
+            std::process::exit(1);
+        }
+    } else {
+        eprintln!("ERROR: Could not read path. Please provide a valid HDR image path.");
+        std::process::exit(1);
+    }
+
 }
