a81c557c7e8b1d604202469c213d32e479ae322c
mandeep <mandeep@users.noreply.github.com>
2020-03-10 17:28:08 -0400

    Create image buffer of exr image and pass into render loop

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,55 @@
 use std::env;
 use std::path::Path;
 
-use image2::io;
+use exr;
+use image2;
 
 mod render;
-use render::render;
+mod utils;
+
+use crate::render::render;
+use crate::utils::extract_exr_data;
 
 fn main() {
-    let file = env::args()
-        .skip(1)
-        .next()
-        .expect("ERROR: A filepath to an image must be provided as a commandline argument.");
+    let file =
+        env::args().skip(1).next().expect("ERROR: A filepath to an image must be provided as a \
+                                           commandline argument.");
 
     let filepath = Path::new(&file);
 
     if filepath.is_file() {
-        if let Ok(image) = io::read(filepath) {
-            if let Err(error) = render(image, filepath) {
-                eprintln!("{}", error);
-                std::process::exit(1);
+        if let Some(extension) = filepath.extension() {
+            if extension == "exr" {
+                if let Ok(exr_image) =
+                    exr::image::rgba::Image::read_from_file(filepath,
+                                                            exr::image::read_options::high())
+                {
+                    let exr_data = extract_exr_data(&exr_image);
+                    let image: image2::ImageBuf<u8, image2::Rgb> = image2::ImageBuf::new_from(exr_image.resolution.0, exr_image.resolution.1, exr_data);
+
+
+                    if let Err(error) = render(image, filepath) {
+                        eprintln!("{}", error);
+                        std::process::exit(1);
+                    }
+
+                } else {
+                    eprintln!("ERROR: Failed to read OpenEXR image. Please make sure it is a \
+                               valid image file.");
+                    std::process::exit(1);
+                }
+            } else {
+                if let Ok(hdr_image) = image2::io::read(filepath) {
+                    if let Err(error) = render(hdr_image, filepath) {
+                        eprintln!("{}", error);
+                        std::process::exit(1);
+                    }
+                } else {
+                    eprintln!("ERROR: Failed to read image. Please make sure it is a valid image \
+                               file.");
+                    std::process::exit(1);
+                }
             }
-        } else {
-            eprintln!("ERROR: Failed to read image. Please make sure it is a valid image file.");
-            std::process::exit(1);
         }
     } else {
         eprintln!("ERROR: Could not read path. Please provide a valid HDR image path.");
