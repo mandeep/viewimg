b7b4b62cebdb27fb34b0a37213dea66aa52328d1
mandeep <mandeep@users.noreply.github.com>
2020-03-31 23:53:02 -0400

    Replace eprint! with eprintln

--- src/macros.rs
+++ src/macros.rs
@@ -3,9 +3,9 @@
 /// Reference: https://www.reddit.com/r/rust/comments/7vveid/unwrapping_with_the_exact_error_message_from_err/
 #[macro_export]
 macro_rules! exit {
-    ($fmt:expr) => ({eprint!(concat!($fmt, "\n")); ::std::process::exit(1)});
+    ($fmt:expr) => ({eprintln!($fmt); std::process::exit(1)});
     ($fmt:expr, $($arg:tt)*) => ({
-            eprint!(concat!($fmt, "\n"), $($arg)*);
-            ::std::process::exit(1)
+            eprintln!($fmt, $($arg)*);
+            std::process::exit(1)
     });
 }
